233cccc96646d5c40e4302872470ee3170ae09f2
unknown <henrique.morano@rocketmail.com>
2020-08-28 17:59:18 -0300

    Comecei a estruturar o código

--- Ramona.py
+++ Ramona.py
@@ -1,12 +1,29 @@
-import logging 
+from os import system, name
+import logging
+from chatterbot import ChatBot
 
-logger = logging.getLogger() 
+logger = logging.getLogger()
 logger.setLevel(logging.CRITICAL)
 
-from chatterbot import ChatBot
+
+def clear():
+
+    if name == 'nt':
+        _ = system('cls')
+
+    else:
+        _ = system('clear')
+
+
 chatbot = ChatBot('Ramona')
 
+clear()
+print("Ramona: Olá")
+
 while True:
+
     quest = input("Você: ").capitalize()
+    if(quest == "sair".capitalize()):
+        break
     response = chatbot.get_response(quest)
     print('Ramona: ', response)
